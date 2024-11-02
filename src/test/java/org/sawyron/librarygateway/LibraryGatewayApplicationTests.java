2378a63c70c3165af62188ac6a51141be44b4915
Sawyron <s4pakvv@gmail.com>
2024-11-02 17:07:30 +0700

    initial commit

---
+++ src/test/java/org/sawyron/librarygateway/LibraryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sawyron.librarygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
